a514260d67ca953e7f3b2bf3fe26319a58b1a995
Anatolii Tserkunyk <cerkunik@gmail.com>
2024-12-03 17:45:16 +0200

    added backend url

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:5000';
+// export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://pokemon-backend-dlbr.onrender.com/';
 
 const AUTH_ROUTE = '/auth';
 const POKEMON_ROUTE = '/pokemon';
